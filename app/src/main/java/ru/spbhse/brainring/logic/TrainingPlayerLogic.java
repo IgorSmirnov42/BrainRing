c60c5ec010f8058554e4346b661c14eb9b249cfa
Serg Kuzmin <serg.kuzmin00@mail.ru>
2019-05-16 15:47:48 +0300

    comment bad string

--- app/src/main/java/ru/spbhse/brainring/logic/TrainingPlayerLogic.java
+++ app/src/main/java/ru/spbhse/brainring/logic/TrainingPlayerLogic.java
@@ -129,7 +129,7 @@ public class TrainingPlayerLogic {
             player.setOnCompletionListener(MediaPlayer::release);
             player.start();
         }).start();
-        TrainingController.TrainingUIController.setTime(String.valueOf(secondsLeft));
+//        TrainingController.TrainingUIController.setTime(String.valueOf(secondsLeft));
     }
 
     public void answerButtonPushed() {
